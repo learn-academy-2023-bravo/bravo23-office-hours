88148835875723094928f08a1e29a8f889df0a7a
Charlean Baxter <baxtercm@outlook.com>
2023-04-05 17:28:17 -0700

    landing page

---
+++ react-projects/inputs-week4-office/src/App.js
@@ -0,0 +1,18 @@
+// import
+import React from "react"
+
+// functional component declaration
+const App = () => {
+  // state values, inputs, and event listener method
+
+  // return to the UI
+  return(
+    <>
+      <h1>Once, Twice, Three Times a Robot</h1>
+      <input />
+    </>
+  )
+}
+
+// export
+export default App
